deac1bafc7fb98d180276c7d85d2d5d29f90416d
Gene Harold <geneisme20@gmail.com>
2024-09-25 21:28:13 +0800

    made a table

--- EnrollmentSystemUCB/Data/ApplicationDbContext.cs
+++ EnrollmentSystemUCB/Data/ApplicationDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using EnrollmentSystemUCB.Models.Entities;
 
 namespace EnrollmentSystemUCB.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
 
+        public DbSet<Student> Students { get; set; }
     }
 }
