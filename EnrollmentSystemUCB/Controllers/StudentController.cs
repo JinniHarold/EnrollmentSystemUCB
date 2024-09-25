ee439a0b9007670320e640a2e8594721e4edee37
Gene Harold <geneisme20@gmail.com>
2024-09-25 22:29:49 +0800

    Save Data to DB!!

--- EnrollmentSystemUCB/Controllers/StudentController.cs
+++ EnrollmentSystemUCB/Controllers/StudentController.cs
@@ -1,18 +1,39 @@
-﻿using EnrollmentSystemUCB.Models;
+﻿using EnrollmentSystemUCB.Data;
+using EnrollmentSystemUCB.Models;
+using EnrollmentSystemUCB.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnrollmentSystemUCB.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Add(AddStudentViewModel viewModel)
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
         {
+            var student = new Student
+            {
+                StudFName = viewModel.StudFName,
+                StudMInitial = viewModel.StudMInitial,
+                StudLName = viewModel.StudLName,
+                StudCourse = viewModel.StudCourse,
+                StudYear = viewModel.StudYear,
+            };
+
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+
             return View();
         }
     }
