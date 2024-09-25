837d2496a8b95542be0224c4a54411c456c34ab1
Gene Harold <geneisme20@gmail.com>
2024-09-26 00:32:12 +0800

    Final Save karon adlawa

--- EnrollmentSystemUCB/Controllers/StudentController.cs
+++ EnrollmentSystemUCB/Controllers/StudentController.cs
@@ -73,5 +73,18 @@ namespace EnrollmentSystemUCB.Controllers
             }
             return RedirectToAction("List", "Student");
         }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel) 
+        {
+            var student = await dbContext.Students.AsNoTracking().FirstOrDefaultAsync(x => x.Id == viewModel.Id);
+
+            if (student is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Student");
+        }
     }
 }
