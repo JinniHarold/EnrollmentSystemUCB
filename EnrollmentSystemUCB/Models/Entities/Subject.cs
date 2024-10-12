bb2b1ff4e376402f6cb6c2ccbd1a36664c9dd6c1
Gene Harold <geneisme20@gmail.com>
2024-10-12 18:02:33 +0800

    student cs

---
+++ EnrollmentSystemUCB/Models/Entities/Subject.cs
@@ -0,0 +1,10 @@
+﻿namespace EnrollmentSystemUCB.Models.Entities
+{
+    public class Subject
+    {
+        public string SubjectCode { get; set; }
+        public string SubjectDescription { get; set; }
+        public int SubjectUnits { get; set; }
+        public int SubjectCategory { get; set; }
+    }
+}
