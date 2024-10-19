7556cf266b9969fa19e068b240955f88091954a2
Gene Harold <geneisme20@gmail.com>
2024-10-19 14:32:55 +0800

    di pa sya masave sa db :(

--- EnrollmentSystemUCB/Controllers/SubjectScheduleController.cs
+++ EnrollmentSystemUCB/Controllers/SubjectScheduleController.cs
@@ -20,20 +20,19 @@ namespace EnrollmentSystemUCB.Controllers
             // Find the subject with the provided subjectCode from the Subjects table
             var subject = await dbContext.Subjects.FirstOrDefaultAsync(s => s.SubjectCode == subjectCode);
 
-            // If the subject doesn't exist, return to the subject list or handle error
+            
             if (subject == null)
             {
-                return RedirectToAction("ListSubject", "Subject"); // Redirect back to list if not found
+                return RedirectToAction("ListSubject", "Subject");
             }
 
-            // Create the view model with the subject's code and description pre-filled
             var viewModel = new AddSubjectScheduleViewModel
             {
                 SubjectCode = subject.SubjectCode,
                 SUbjectDescription = subject.SubjectDescription
             };
 
-            return View(viewModel); // Pass the view model to the view
+            return View(viewModel); 
         }
         [HttpPost]
         public async Task<IActionResult> AddSubjectSchedule(AddSubjectScheduleViewModel viewModel)
@@ -43,10 +42,9 @@ namespace EnrollmentSystemUCB.Controllers
                 return View(viewModel);
             }
 
-            // Create a new SubjectSchedule entity from the view model
             var subjectSched = new SubjectSchedule
             {
-                SubjectEDPCode = viewModel.SubjectEDPCode, // This should be filled if necessary
+                SubjectEDPCode = viewModel.SubjectEDPCode,
                 SubjectCode = viewModel.SubjectCode,
                 SUbjectDescription = viewModel.SUbjectDescription,
                 SubjectTimeStart = viewModel.SubjectTimeStart,
@@ -56,11 +54,9 @@ namespace EnrollmentSystemUCB.Controllers
                 SubjectSY = viewModel.SubjectSY
             };
 
-            // Save the SubjectSchedule entity to the database
             await dbContext.SubjectSchedules.AddAsync(subjectSched);
             await dbContext.SaveChangesAsync();
 
-            // Redirect to the list of subjects or any other relevant page after saving
             return RedirectToAction("ListSubject", "Subject");
         }
     }
